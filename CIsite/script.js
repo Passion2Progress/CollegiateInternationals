1657312b80775898c634fa1002bac925690e02de
stephanrb3 <stephanrb3@gmail.com>
2018-02-03 19:51:14 -0500

    Site Updated Fixed Grid

--- CIsite/script.js
+++ CIsite/script.js
@@ -205,9 +205,6 @@ function searchVersion(pstrDataString)
 }
 
 
-// groups dropdown
-
-
 function plusToggle(){
 	$('.fa').click(function () {
         $(this).toggleClass("fa-plus-circle").toggleClass("fa-minus-circle");
@@ -216,7 +213,7 @@ function plusToggle(){
 
 function ExpandGroup() 
 {
-	var x = document.getElementById('PTK');
+	var x = document.getElementById('group');
     if (x.style.display && x.style.display !== 'none') {
         x.style.display = 'none';
     } else {
@@ -224,243 +221,154 @@ function ExpandGroup()
     }
 }	
 
-function ExpandGroup2() 
-{
-	var x = document.getElementById('SAGA');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandGroup3() 
-{
-	var x = document.getElementById('SCconnect');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
 
-function ExpandGroup4() 
-{
-	var x = document.getElementById('MPC');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandGroup5() 
-{
-	var x = document.getElementById('VPC');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+(function($) {
+	$.StartScreen = function(){
+		var plugin = this;
+		var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+		
+		var setTilesAreaSize = function(){
+			var groups = $(".tile-group");
+			var tileAreaWidth = 80;
+			$.each(groups, function(i, t){
+				if (width <= 640) {
+					tileAreaWidth = width;
+				} else {
+					tileAreaWidth += $(t).outerWidth() + 80;
+				}
+			});
+			$(".tile-area").css({
+				width: tileAreaWidth
+			});
+		};
 
-function ExpandGroup6() 
-{
-	var x = document.getElementById('SAB');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-// function ExpandGroup7() 
-// {
-// 	var x = document.getElementById('');
-//     if (x.style.display === 'none') {
-//         x.style.display = 'block';
-//     } else {
-//         x.style.display = 'none';
-//     }
-// }	
 
-// function ExpandGroup8() 
-// {
-// 	var x = document.getElementById('');
-//     if (x.style.display === 'none') {
-//         x.style.display = 'block';
-//     } else {
-//         x.style.display = 'none';
-//     }
-// }	
+		plugin.init();
+	}
+})(jQuery);
 
-function ExpandSkill() 
-{
-	var x = document.getElementById('Csharp');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+$(function(){
+	$.StartScreen();
 
-function ExpandGroup2() 
-{
-	var x = document.getElementById('SAGA');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandGroup3() 
-{
-	var x = document.getElementById('SCconnect');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+	var tiles = $(".tile, .tile-small, .tile-sqaure, .tile-wide, .tile-large, .tile-big, .tile-super");
 
-function ExpandGroup4() 
-{
-	var x = document.getElementById('MPC');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandGroup5() 
-{
-	var x = document.getElementById('VPC');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+	$.each(tiles, function(){
+		var tile = $(this);
+		setTimeout(function(){
+			tile.css({
+				opacity: 1,
+				"-webkit-transform": "scale(1)",
+				"transform": "scale(1)",
+				"-webkit-transition": ".3s",
+				"transition": ".3s"
+			});
+		}, Math.floor(Math.random()*500));
+	});
 
-function ExpandGroup6() 
-{
-	var x = document.getElementById('SAB');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+	$(".tile-group").animate({
+		left: 0
+	});
+});
 
-//expand skills
-function ExpandSkill() 
-{
-	var x = document.getElementById('Csharp');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+function showCharms(id){
+	var  charm = $(id).data("charm");
+	if (charm.element.data("opened") === true) {
+		charm.close();
+	} else {
+		charm.open();
+	}
+}
 
-function ExpandSkill2() 
-{
-	var x = document.getElementById('Illi');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandSkill3() 
-{
-	var x = document.getElementById('HTML');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+function setSearchPlace(el){
+	var a = $(el);
+	var text = a.text();
+	var toggle = a.parents('label').children('.dropdown-toggle');
 
-function ExpandSkill4() 
-{
-	var x = document.getElementById('Psb');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandSkill5() 
-{
-	var x = document.getElementById('Cplus');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+	toggle.text(text);
+}
 
-function ExpandSkill6() 
-{
-	var x = document.getElementById('Unity');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandSkill7() 
-{
-	var x = document.getElementById('Casss');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+$(function(){
+	var current_tile_area_scheme = localStorage.getItem('tile-area-scheme') || "tile-area-scheme-dark";
+	$(".tile-area").removeClass (function (index, css) {
+		return (css.match (/(^|\s)tile-area-scheme-\S+/g) || []).join(' ');
+	}).addClass(current_tile_area_scheme);
 
-function ExpandSkill8() 
-{
-	var x = document.getElementById('VS');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandSkill9() 
-{
-	var x = document.getElementById('OOP');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+	$(".schemeButtons .button").hover(
+			function(){
+				var b = $(this);
+				var scheme = "tile-area-scheme-" +  b.data('scheme');
+				$(".tile-area").removeClass (function (index, css) {
+					return (css.match (/(^|\s)tile-area-scheme-\S+/g) || []).join(' ');
+				}).addClass(scheme);
+			},
+			function(){
+				$(".tile-area").removeClass (function (index, css) {
+					return (css.match (/(^|\s)tile-area-scheme-\S+/g) || []).join(' ');
+				}).addClass(current_tile_area_scheme);
+			}
+	);
 
-function ExpandSkill10() 
-{
-	var x = document.getElementById('PM');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
-function ExpandSkill12() 
-{
-	var x = document.getElementById('JS');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+	$(".schemeButtons .button").on("click", function(){
+		var b = $(this);
+		var scheme = "tile-area-scheme-" +  b.data('scheme');
 
-function ExpandSkill13() 
-{
-	var x = document.getElementById('PY');
-    if (x.style.display && x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}	
+		$(".tile-area").removeClass (function (index, css) {
+			return (css.match (/(^|\s)tile-area-scheme-\S+/g) || []).join(' ');
+		}).addClass(scheme);
+
+		current_tile_area_scheme = scheme;
+		localStorage.setItem('tile-area-scheme', scheme);
+
+		showSettings();
+	});
+});
+
+var weather_icons = {
+	'clear-day': 'mif-sun',
+	'clear-night': 'mif-moon2',
+	'rain': 'mif-rainy',
+	'snow': 'mif-snowy3',
+	'sleet': 'mif-weather4',
+	'wind': 'mif-wind',
+	'fog': 'mif-cloudy2',
+	'cloudy': 'mif-cloudy',
+	'partly-cloudy-day': 'mif-cloudy3',
+	'partly-cloudy-night': 'mif-cloud5'
+};
+
+var api_key = 'AIzaSyDPfgE0qhVmCcy-FNRLBjO27NbVrFM2abg';
+
+$(function(){
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+			var lat = position.coords.latitude, lon = position.coords.longitude;
+			var pos = lat+','+lon;
+			var latlng = new google.maps.LatLng(lat, lon);
+			var geocoder = new google.maps.Geocoder();
+			$.ajax({
+				url: '//api.forecast.io/forecast/219588ba41dedc2f1019684e8ac393ad/'+pos+'?units=si',
+				dataType: 'jsonp',
+				success: function(data){
+					//do whatever you want with the data here
+					geocoder.geocode({latLng: latlng}, function(result, status){
+						console.log(result[3]);
+						$("#city_name").html(result[3].formatted_address);
+					});
+					var current = data.currently;
+					//$('#city_name').html(response.city+", "+response.country);
+					$("#city_temp").html(Math.round(current.temperature)+" &deg;C");
+					$("#city_weather").html(current.summary);
+					$("#city_weather_daily").html(data.daily.summary);
+					$("#weather_icon").addClass(weather_icons[current.icon]);
+					$("#pressure").html(current.pressure);
+					$("#ozone").html(current.ozone);
+					$("#wind_bearing").html(current.windBearing);
+					$("#wind_speed").html(current.windSpeed);
+					$("#weather_bg").css({
+						'background-image': 'url(../images/'+current.icon+'.jpg'+')'
+					});
+				}
+			});
+		});
+	}
+});
